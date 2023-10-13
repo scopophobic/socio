401c7d94b07090923eac9ca39fc8ebb7750e1643
scopophobic <adithyanmadhu1234@hmail.com>
2023-10-13 14:39:20 +0530

    added railway to host

--- socio/settings.py
+++ socio/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['socio-production.up.railway.app','127.0.0.1']
 
 
 # Application definition
